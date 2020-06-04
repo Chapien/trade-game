fea894a26dc95403276d152aaa5b7b1f9d9fcb43
Claire Robsahm <claire@chapien.net>
2020-06-04 00:40:45 -0700

    More baseline work.

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,40 @@
 extern crate amethyst;
 
-use amethyst::{Application, Duration, State, Trans};
+use amethyst::{
+    prelude::*,
+    renderer::{
+        plugins::{RenderFlat2D, RenderToWindow},
+        types::DefaultBackend,
+        RenderingBundle,
+    },
+    utils::application_root_dir,
+};
 
-struct HelloWorld;
+pub struct Game;
 
-impl State for HelloWorld {
-    fn on_start(&mut self) {
-        println!("Game started!");
-    }
+impl SimpleState for Game {}
 
-    fn update(&mut self, _delta: Duration) -> Trans {
-        println!("Hello from Amethyst!");
-        Trans::Quit
-    }
+fn main() -> amethyst::Result<()> {
 
-    fn on_stop(&mut self) {
-        println!("Game stopped!");
-    }
-}
+    // We'll put the rest of the code here.
+    amethyst::start_logger(Default::default());
+    let app_root = application_root_dir()?;
+    let display_config_path = app_root.join("config").join("display.ron");
 
-fn main() {
-    let mut game = Application::new(HelloWorld);
+    let game_data = GameDataBuilder::default()
+    .with_bundle(
+        RenderingBundle::<DefaultBackend>::new()
+            // The RenderToWindow plugin provides all the scaffolding for opening a window and drawing on it
+            .with_plugin(
+                RenderToWindow::from_config_path(display_config_path)?
+                    .with_clear([0.0, 0.0, 0.0, 1.0]),
+            )
+            // RenderFlat2D plugin is used to render entities with a `SpriteRender` component.
+            .with_plugin(RenderFlat2D::default()),
+    )?;
+
+    let assets_dir = app_root.join("assets");
+    let mut game = Application::new(assets_dir, Game, game_data)?;
     game.run();
+    Ok(())
 }
